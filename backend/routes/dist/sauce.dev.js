a54ebc71b3aba6d8b2ed40245bc55901850847a1
Elodie-Pesselon <74595093+Elodie-Pesselon@users.noreply.github.com>
2021-05-05 10:50:35 +0200

    Création dossier route + ajout fichier sauce.js + user.js

---
+++ backend/routes/dist/sauce.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var express = require('express');
+
+var router = express.Router();
+
+var auth = require('../middleware/auth');
+
+var multer = require('../middleware/multer-config');
+
+var sauceCtrl = require('../controllers/sauce');
+
+router.get('/', auth, sauceCtrl.getAllSauces);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router["delete"]('/:id', auth, sauceCtrl.deleteSauce);
+module.exports = router;
